b42e79d987f9e8f3b78f5187339401c5c982cc49
verve-neowise <verve.neowise@gmail.com>
2022-05-08 14:15:49 +0500

    react-todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,64 @@
-import { useState } from "react";
-import Todo from "./Components/Todo";
-import { TodoList } from "./Components/TodoList";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-	const [todos, setTodos] = useState([])
+  const submitHandler = (event) => {
+    event.preventDefault();
+    
+    if(!value.trim()) return alert("Todo kiriting")
 
-	const deleteTodo = (id) => {
-		let newArray = todos.filter(todos => {
-			return todos.id !== id
-		})
-		setTodos(newArray)
-	}
-		return (
-			<div className="App">
-				<Todo setTodos={setTodos} />
-				<TodoList deleteTodo={deleteTodo} todos={todos} />
-			</div>
-		);
-	}
+    let oldTodos = localStorage.getItem("todo")
+      ? JSON.parse(localStorage.getItem("todo"))
+      : [];
+
+    const newTodo = {
+      id: Date.now(),
+      text: value,
+    };
+    localStorage.setItem("todo", JSON.stringify([...oldTodos, newTodo]));
+    setLoading(!loading);
+    setValue("")
+  };
+
+  useEffect(() => {
+    let todos = localStorage.getItem("todo")
+      ? JSON.parse(localStorage.getItem("todo"))
+      : [];
+    setTodos(todos);
+  }, [loading]);
+
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    localStorage.setItem("todo", JSON.stringify(newTodos))
+    setLoading(!loading)
+  }
+
+  return (
+    <div className="App">
+		<h1>TODO</h1>
+      <form onSubmit={submitHandler} style={{ width: 500, margin: "0 auto" }}>
+        <input
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          type="text"
+          placeholder="todo"
+        />
+        <button className="btn">ADD</button>
+      </form>
+
+      <ul>
+        {todos.map((todo, index) => (
+		  <div className="todo">
+          <li key={index}>{todo.text} </li>
+		  <button className="x" onClick={() => deleteTodo(todo.id)}>X</button>
+		  </div>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
